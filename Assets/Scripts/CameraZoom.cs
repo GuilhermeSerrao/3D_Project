e8171bcf9769c6f282b7f6e41fd51728496d1932
Guilherme <guilherme-serrao.dev@outlook.pt>
2020-03-12 10:39:52 +0000

    CameraTweaks

---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+
+    [SerializeField]
+    private float zoomValue;
+
+    [SerializeField]
+    private float zoomSpeed;
+
+    [SerializeField]
+    private float zoomPosition;
+
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float scroll = Input.GetAxisRaw("Mouse ScrollWheel");
+
+        if (scroll > 0)
+        {
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, cam.orthographicSize -= zoomValue, zoomSpeed * Time.deltaTime);
+            cam.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y - (zoomValue/ zoomPosition), cam.transform.position.z);
+        }
+        else if (scroll <  0)
+        {
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, cam.orthographicSize += zoomValue, zoomSpeed * Time.deltaTime);
+            cam.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y + (zoomValue / zoomPosition), cam.transform.position.z);
+        }
+    }
+
+    
+}
