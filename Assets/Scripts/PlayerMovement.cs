e85450b9036680c3c8c0ae65bdcc465900962d27
Guilherme <guilherme-serrao.dev@outlook.pt>
2020-03-12 21:52:28 +0000

    testes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,6 +15,9 @@ public class PlayerMovement : MonoBehaviour
     private Transform camera;
 
     [SerializeField]
+    private float gravity;
+
+    [SerializeField]
     private PhysicMaterial groundedMaterial;
 
     [SerializeField]
@@ -30,8 +33,13 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody rb;
 
+    private RaycastHit groundHit;
+
+    private Collider coll;
+
     private void Awake()
-    {        
+    {
+        coll = GetComponent<Collider>();
         rb = GetComponent<Rigidbody>();
         distanceGround = GetComponent<Collider>().bounds.extents.y;
     }
@@ -42,25 +50,36 @@ public class PlayerMovement : MonoBehaviour
         airSpeed = moveSpeed / 2;
     }
 
+    private void Update()
+    {
+
+        if (Physics.CapsuleCast(transform.position + Vector3.up * 0.5f, transform.position, 0.4f, transform.TransformDirection(Vector3.down), out groundHit, Mathf.Infinity))
+        {
+            Debug.Log(groundHit.distance);
+            if (groundHit.distance <= 0.7f)
+            {
+                isGrounded = true;
+                coll.material = groundedMaterial;                
+                moveSpeed = originalSpeed;
+            }
+            else
+            {
+                isGrounded = false;
+                coll.material = airMaterial;
+                moveSpeed = airSpeed;
+            }
+                
+        }
+
+    }
+
     private void FixedUpdate()
     {
         float verticalAxis = Input.GetAxisRaw("Vertical");
         float horizontalAxis = Input.GetAxisRaw("Horizontal");
         float jumpAxis = Input.GetAxisRaw("Jump");       
 
-        if (!Physics.Raycast(transform.position, -Vector3.up, distanceGround + 0.5f))
-        {
-            isGrounded = false;
-            moveSpeed = airSpeed;
-            GetComponent<Collider>().material = airMaterial;
-
-        }
-        else
-        {
-            isGrounded = true;
-            moveSpeed = originalSpeed;
-            GetComponent<Collider>().material = groundedMaterial;
-        }
+       
 
         Vector3 camf = camera.forward;
         Vector3 camR = camera.right;
@@ -75,9 +94,15 @@ public class PlayerMovement : MonoBehaviour
 
         rb.MovePosition(transform.position + direction * (moveSpeed * Time.deltaTime));
 
-        if (isGrounded && jumpAxis != 0)
+        if (!isGrounded)
         {
-            rb.AddForce(new Vector3(rb.velocity.x, jumpAxis * jumpForce, rb.velocity.z), ForceMode.Force);
+            rb.AddForce(Vector3.down * gravity);
+        }
+
+        if (isGrounded && jumpAxis != 0 && rb.velocity.y <= 0)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpAxis * jumpForce, rb.velocity.z);
+            //rb.AddForce(new Vector3(rb.velocity.x, jumpAxis * jumpForce, rb.velocity.z), ForceMode.VelocityChange);
         }
     }
 }
