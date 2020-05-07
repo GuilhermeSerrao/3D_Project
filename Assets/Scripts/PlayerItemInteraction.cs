c4ad8f34393dba055409d1cacfe9f64955187812
Guilherme <guilherme-serrao.dev@outlook.pt>
2020-05-07 05:39:37 +0100

    PickUp

---
+++ Assets/Scripts/PlayerItemInteraction.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerItemInteraction : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform leftHand, rightHand;
+
+    [SerializeField]
+    private Rigidbody leftJoint, rightJoint;
+
+    private bool leftHandFree = true, rightHandFree = true;
+
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("PickUpTrash") && Input.GetKeyDown(KeyCode.E) && (rightHand || leftHand))
+        {
+            other.gameObject.layer = 14;
+            var rbody = other.GetComponent<Rigidbody>();
+            rbody.isKinematic = true;
+            rbody.useGravity = false;
+
+            FixedJoint fixedJoint = other.gameObject.AddComponent<FixedJoint>();
+
+
+            if (rightHandFree)
+            {
+                rightHandFree = false;
+                other.transform.position = rightHand.position;
+                other.transform.parent = rightHand;
+                fixedJoint.anchor = rightHand.position;
+                fixedJoint.connectedAnchor = rightJoint.position;
+
+            }
+            else if (leftHandFree)
+            {
+                leftHandFree = false;
+                other.transform.position = leftHand.position;
+                other.transform.parent = leftHand;
+                fixedJoint.anchor = leftHand.position;
+                fixedJoint.connectedAnchor = leftJoint.position;
+            }
+        }
+
+        if (other.CompareTag("Tool") && Input.GetKeyDown(KeyCode.E) && (rightHand || leftHand))
+        {
+            var rbody = other.GetComponent<Rigidbody>();
+            rbody.isKinematic = true;
+            rbody.useGravity = false;
+
+            FixedJoint fixedJoint = other.gameObject.AddComponent<FixedJoint>();            
+           
+
+            if (rightHandFree)
+            {
+                rightHandFree = false;
+                other.transform.position = rightHand.position;
+                other.transform.parent = rightHand;
+                fixedJoint.anchor = rightHand.position;
+                fixedJoint.connectedAnchor = rightJoint.position;
+                
+            }
+        }
+    }
+
+ 
+}
---
+++ Assets/Scripts/SpawnObjectsToClean.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SpawnObjectsToClean : MonoBehaviour
+{
+    
+    private List<GameObject> liquidSpawnLocations, trashSpawnLocations;
+
+    [SerializeField]
+    private GameObject[] liquidsToSpawn, trashToSpawn;
+
+    [SerializeField]
+    private int maxLiquidsSpawned, maxTrashSpawned;
+
+    private int liquidsSpawned = 0, trashSpawned = 0;
+
+    private void Awake()
+    {
+        liquidSpawnLocations = GameObject.FindGameObjectsWithTag("LiquidSpawn").ToList();
+        trashSpawnLocations = GameObject.FindGameObjectsWithTag("TrashSpawn").ToList();
+    }
+    void Start()
+    {
+        for (liquidsSpawned = 0; liquidsSpawned < maxLiquidsSpawned; liquidsSpawned++)
+        {
+            var location = Random.Range(0, liquidSpawnLocations.Count);
+            Instantiate(liquidsToSpawn[Random.Range(0, liquidsToSpawn.Length)], liquidSpawnLocations[location].transform);
+            liquidSpawnLocations.Remove(liquidSpawnLocations[location]);
+        }
+
+        for (trashSpawned = 0; trashSpawned < maxTrashSpawned; trashSpawned++)
+        {
+            var location = Random.Range(0, trashSpawnLocations.Count);
+            Instantiate(trashToSpawn[Random.Range(0, trashToSpawn.Length)], trashSpawnLocations[location].transform);
+            trashSpawnLocations.Remove(trashSpawnLocations[location]);
+        }
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
